f51522851e4ec85f24c16034d4bc9c7c2aa31946
etiennefaivredarcier <etienne.faivredarcier@interdigital.com>
2024-03-26 17:54:32 +0100

    Added missing ARUtilities.cs

---
+++ Runtime/Scripts/ARUtilities.cs
@@ -0,0 +1,42 @@
+using System;
+using Unity.XR.CoreUtils;
+using UnityEngine;
+
+/// <summary>
+/// Utility class to handle version references
+/// and a few handy methods 
+/// </summary>
+public static class ARUtilities
+{
+    /// <summary>
+    /// Returns the XR Origin or the ARSessionOrigin in the scene
+    /// throw an exception if it doesn't exists
+    /// </summary>
+    /// <returns></returns>
+    public static GameObject GetSessionOrigin()
+    {
+// ARSessionOrigin is deprecated in version 2022.3 or above
+        GameObject go = null;
+#if UNITY_2022_3_OR_NEWER
+        XROrigin or = GameObject.FindObjectOfType<XROrigin>();
+        if(or == null)
+        {
+            throw new Exception("No XR Origin found");
+        }
+        go = or.gameObject;
+#else
+        ARSessionOrigin arSess = GameObject.FindObjectOfType<ARSessionOrigin>();
+        if(arSess == null)
+        {
+            throw new Exception("No AR Session origin found");
+        }
+        go = arSess.gameObject;
+#endif
+        if(go == null)
+        {
+            throw new Exception("Can't initialize Trackable marker geo, no origin found");
+        }
+
+        return go;
+    }
+}
